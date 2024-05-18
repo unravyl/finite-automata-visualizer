0324c64de630db90c6bc5ea62bfcf874d35e4b20
Maxell Milay <milaymaxell@gmail.com>
2024-05-18 10:20:33 +0800

    [Adjustment][Maxell] removed an import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react'
-import RegularExpression from '../classes/RegularExpression'
 import SyntaxTree from '../classes/SyntaxTree'
 
 export default function Page() {
