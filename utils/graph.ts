ed4193fa1531261cc1100dec959844822895470b
Maxell Milay <milaymaxell@gmail.com>
2024-05-25 22:16:58 +0800

    [Feature][Maxell] fixed node and link generator and added dead states

--- utils/graph.ts
+++ utils/graph.ts
@@ -5,6 +5,7 @@ export interface NodeInterface extends SimulationNodeDatum {
     id: number;
     values: number[];
     group: number;
+    isFinalState: boolean;
 }
 
 export interface LinkInterface
@@ -14,12 +15,10 @@ export interface LinkInterface
     transition: string;
 }
 
-const findNodeByTargetValues = (
+export const findNodeByTargetValues = (
     target: number[],
     nodes: NodeInterface[]
 ): NodeInterface => {
-    const targetSet = new Set(target);
-
     for (const node of nodes) {
         const nodeSet = new Set(node.values);
         if (target.every((val) => nodeSet.has(val))) {
@@ -30,7 +29,7 @@ const findNodeByTargetValues = (
     return null;
 };
 
-const getNewNodes = (
+export const getNewNodes = (
     currentNode: NodeInterface,
     followpos: FollowposResult[]
 ) => {
@@ -65,7 +64,10 @@ const getNewNodes = (
     return { a, b };
 };
 
-const isArrayPresent = (target: number[], base: NodeInterface[]): boolean => {
+export const isArrayPresent = (
+    target: number[],
+    base: NodeInterface[]
+): boolean => {
     const stringifiedTarget = JSON.stringify(target.sort());
     return base.some((node) => {
         const stringifiedValues = JSON.stringify(node.values.sort());
@@ -73,39 +75,111 @@ const isArrayPresent = (target: number[], base: NodeInterface[]): boolean => {
     });
 };
 
-const generateLink = (source, target, transition) => {
+export const generateLink = (source, target, transition) => {
     return { source, target, transition };
 };
 
-const generateNode = (id, values, group) => {
-    return { id, values, group };
+export const generateNode = (id, values, group, finalState) => {
+    return { id, values, group, isFinalState: values.includes(finalState) };
+};
+
+export const getNewValues = (
+    potentialList: number[],
+    followpos: FollowposResult[]
+) => {
+    let newValues = [];
+
+    potentialList.forEach((id) => {
+        const matchedFollowPos = followpos.find((value) => {
+            return id === value.number;
+        });
+
+        if (matchedFollowPos) {
+            newValues.push(...matchedFollowPos.followpos);
+        }
+    });
+
+    return newValues.filter((item, index) => {
+        return newValues.indexOf(item) === index;
+    });
 };
 
 export const generateNodesAndLinks = (
     firstpos: number[],
     followpos: FollowposResult[]
 ) => {
-    let nodes: NodeInterface[] = [{ id: 1, values: firstpos, group: 1 }];
+    let nodes: NodeInterface[] = [
+        { id: 1, values: firstpos, group: 1, isFinalState: false },
+    ];
     let links: LinkInterface[] = [];
-    let queue: NodeInterface[] = [{ id: 1, values: firstpos, group: 1 }];
+    let queue: NodeInterface[] = [...nodes];
+
+    const finalState = followpos.find((data) => {
+        return data.symbol === '#';
+    }).number;
+
+    let deadState = null;
 
     let nodeCount = 2;
 
     while (queue.length > 0) {
-        const currentNode = queue.pop();
+        const currentNode = queue.shift();
+
+        const currentSymbol = followpos[currentNode.id - 1].symbol;
 
         const { a, b } = getNewNodes(currentNode, followpos);
 
-        const potentialNewNodes = [
-            {
+        const potentialNewNodes = [];
+
+        if (a.length !== 0) {
+            potentialNewNodes.push({
                 transition: 'a',
                 list: a,
-            },
-            {
+            });
+        } else if (currentSymbol !== '#') {
+            if (deadState === null) {
+                const newDeadState = generateNode(
+                    nodeCount,
+                    [nodeCount],
+                    1,
+                    finalState
+                );
+                nodeCount += 1;
+                deadState = newDeadState;
+                nodes.push(deadState);
+                const deadA = generateLink(deadState, deadState, 'a');
+                const deadB = generateLink(deadState, deadState, 'b');
+                links.push(deadA, deadB);
+            }
+            const newLink = generateLink(currentNode, deadState, 'a');
+            links.push(newLink);
+        }
+
+        if (b.length !== 0) {
+            potentialNewNodes.push({
                 transition: 'b',
                 list: b,
-            },
-        ];
+            });
+        } else if (currentSymbol !== '#') {
+            if (deadState === null) {
+                const newDeadState = generateNode(
+                    nodeCount,
+                    [nodeCount],
+                    1,
+                    finalState
+                );
+                nodeCount += 1;
+                deadState = newDeadState;
+                nodes.push(deadState);
+                const deadA = generateLink(deadState, deadState, 'a');
+                const deadB = generateLink(deadState, deadState, 'b');
+                links.push(deadA, deadB);
+            }
+            const newLink = generateLink(currentNode, deadState, 'b');
+            links.push(newLink);
+        }
+
+        console.log('LOG POTENTIAL', currentNode.id, potentialNewNodes);
 
         potentialNewNodes.forEach((potential) => {
             if (isArrayPresent(potential.list, nodes)) {
@@ -120,10 +194,18 @@ export const generateNodesAndLinks = (
                 );
                 links.push(newLink);
             } else {
-                const newNode = generateNode(nodeCount, potential.list, 1);
+                // const newValues = getNewValues(potential.list, followpos);
+                // console.log(newValues);
+                const newNode = generateNode(
+                    nodeCount,
+                    potential.list,
+                    1,
+                    finalState
+                );
                 nodeCount += 1;
                 nodes.push(newNode);
                 queue.push(newNode);
+                console.log('LOG LINK TO ADD', currentNode.id, newNode.id);
                 const newLink = generateLink(
                     currentNode,
                     newNode,
