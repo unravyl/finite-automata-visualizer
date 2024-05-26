8aafbee0191bd2bcdf6a2ad607b4ebc80ba85c09
Maxell Milay <milaymaxell@gmail.com>
2024-05-26 18:41:49 +0800

    [Feature][Maxell] attempted to add interactivity

--- components/DFA.tsx
+++ components/DFA.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
-import ReactFlow, { Background } from 'reactflow';
+'use client';
+
+import React, { useState, useCallback, useEffect } from 'react';
+import ReactFlow, {
+    Background,
+    applyNodeChanges,
+    applyEdgeChanges,
+    Edge,
+    Node,
+    NodeChange,
+    EdgeChange,
+} from 'reactflow';
 import 'reactflow/dist/style.css';
 
 import { NodeInterface, LinkInterface } from '../interfaces/graph';
@@ -27,12 +37,46 @@ const DFA = (props: PropsInterface) => {
                 label,
             },
             position: { x: index * 200, y: index % 2 === 0 ? 100 : 300 },
-        };
+        } as Node;
     });
+
+    const diagramEdges = links.map((link) => {
+        const edgeId = `${link.transition}-(${link.source.id})-(${link.target.id})`;
+        return {
+            id: edgeId,
+            source: link.source.id.toString(),
+            target: link.target.id.toString(),
+            label: link.transition,
+        } as Edge;
+    });
+
+    console.log(diagramNodes);
+    console.log(diagramEdges);
+
+    const [nodeState, setNodeState] = useState(diagramNodes);
+    const [edgeState, setEdgeState] = useState(diagramEdges);
+
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) =>
+            setNodeState((nds) => applyNodeChanges(changes, nds)),
+        []
+    );
+
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) =>
+            setEdgeState((eds) => applyEdgeChanges(changes, eds)),
+        []
+    );
 
     return (
         <div className="h-lvh w-full">
-            <ReactFlow nodes={diagramNodes} fitView>
+            <ReactFlow
+                nodes={nodeState}
+                onNodesChange={onNodesChange}
+                edges={edgeState}
+                onEdgesChange={onEdgesChange}
+                fitView
+            >
                 <Background />
             </ReactFlow>
         </div>
