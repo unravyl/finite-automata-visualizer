d91486a02266449a2fe4ecff9c0cfab965b7dd5d
Maxell Milay <milaymaxell@gmail.com>
2024-05-26 18:01:23 +0800

    [Adjustment][Maxell] added new diagram nodes

--- components/DFA.tsx
+++ components/DFA.tsx
@@ -1,106 +1,42 @@
 import React from 'react';
+import ReactFlow, { Background } from 'reactflow';
+import 'reactflow/dist/style.css';
+
 import { NodeInterface, LinkInterface } from '../interfaces/graph';
-import ReactFlow, { MarkerType } from 'react-flow-renderer';
-import CustomNode from './CustomNode';
 
 interface PropsInterface {
     nodes: NodeInterface[];
     links: LinkInterface[];
 }
 
-const getConnectionPoint = (index: number, type: 'source' | 'target') => {
-    if (type === 'source') {
-        return index % 2 === 0 ? 'bottom-source' : 'top-source';
-    }
-
-    return index % 2 === 0 ? 'left-target' : 'right-target';
-};
-
-const GraphSummary = (props: PropsInterface) => {
+const DFA = (props: PropsInterface) => {
     const { nodes, links } = props;
-    const validLinks = Array.isArray(links) ? links : [links];
 
-    const Nodes = Array.isArray(nodes)
-        ? nodes.map((node, index) => ({
-              id: node.id.toString(),
-              type: 'custom',
-              data: {
-                  label: node.isFinalState
-                      ? 'Final'
-                      : node.id === 1
-                        ? 'Start'
-                        : node.id === -1
-                          ? 'Dead'
-                          : `State ${node.id}`,
-              },
-              position: { x: index * 200, y: index % 2 === 0 ? 100 : 300 },
-          }))
-        : [];
-
-    const edges = validLinks.map((link, index) => {
-        const sourcePoint = getConnectionPoint(index, 'source');
-        const targetPoint = getConnectionPoint(index, 'target');
-        const isVerticalEdge =
-            sourcePoint.includes('top') || sourcePoint.includes('bottom');
-
-        const labelPosition = isVerticalEdge ? '0%' : '50%';
+    const diagramNodes = nodes.map((node, index) => {
+        const label = node.isFinalState
+            ? 'Final'
+            : node.id === 1
+              ? 'Start'
+              : node.id === -1
+                ? 'Dead'
+                : `State ${node.id}`;
 
         return {
-            id: `e${link.source.id}-${link.target.id}`,
-            source: link.source.id.toString(),
-            target: link.target.id.toString(),
-            sourceHandle: sourcePoint,
-            targetHandle: targetPoint,
-            type: 'bezier',
-            animated: true,
-            label: link.transition.toString(),
-            labelPosition: labelPosition,
-            markerEnd: {
-                type: MarkerType.ArrowClosed,
-                width: 20,
-                height: 20,
-                color: '#000000',
+            id: node.id.toString(),
+            data: {
+                label,
             },
-            style: {
-                strokeWidth: 2,
-                stroke: '#000000',
-            },
-            labelStyle: {
-                fontSize: 15,
-                fontWeight: 'bold',
-                background: 'white',
-                padding: '2px',
-                borderRadius: '4px',
-            },
-            labelBgStyle: {
-                fill: 'white',
-                fillOpacity: 0.85,
-                strokeWidth: 0,
-            },
+            position: { x: index * 200, y: index % 2 === 0 ? 100 : 300 },
         };
     });
 
     return (
-        <div className="flex flex-col w-full h-full">
-            <div className="w-full h-full">
-                <ReactFlow
-                    nodes={Nodes}
-                    edges={edges}
-                    nodeTypes={{ custom: CustomNode }}
-                    fitView
-                />
-                <style jsx global>{`
-                    .react-flow__edge-text {
-                        font-size: 15px !important;
-                        font-weight: bold;
-                        background: white !important;
-                        padding: 2px !important;
-                        border-radius: 4px !important;
-                    }
-                `}</style>
-            </div>
+        <div className="h-lvh w-full">
+            <ReactFlow nodes={diagramNodes} fitView>
+                <Background />
+            </ReactFlow>
         </div>
     );
 };
 
-export default GraphSummary;
+export default DFA;
