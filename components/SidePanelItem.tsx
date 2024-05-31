9a9178ab8d6f40d95aa18bad4c827dd42f37978e
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-06-01 03:00:01 +0800

    replace link

--- components/SidePanelItem.tsx
+++ components/SidePanelItem.tsx
@@ -76,7 +76,7 @@ function SidePanelItem(props: SidePanelItemProps) {
                                 <button
                                     onClick={() => {
                                         navigator.clipboard.writeText(
-                                            `http://localhost:3000/?regex=${input.regex}`
+                                            `https://favisualizer.vercel.app/?regex=${input.regex}`
                                         );
                                         // navigator.clipboard.writeText(
                                         //     'https://tinyurl.com/2x6wr6n8'
