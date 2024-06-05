f9e005da2709fecce127f9edcdd7e658b4e30356
Maxell Milay <milaymaxell@gmail.com>
2024-06-05 17:33:16 +0800

    [Adjustment][Maxell] updated bidirectional node styles

--- components/BidirectionalNode.tsx
+++ components/BidirectionalNode.tsx
@@ -2,16 +2,20 @@ import React, { memo } from 'react';
 import { Handle, NodeProps, Position } from 'reactflow';
 
 const style = {
-    padding: 10,
     background: '#fff',
-    border: '1px solid #ddd',
+    width: 75,
+    height: 75,
+    border: '2px solid #ddd',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
 };
 
 const BiDirectionalNode = ({ data }: NodeProps) => {
     return (
         <div style={style}>
             <Handle type="source" position={Position.Left} id="left" />
-            {data?.label}
+            {data.label}
             <Handle type="source" position={Position.Right} id="right" />
         </div>
     );
