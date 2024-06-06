df0138a65081c94efab2fed13502de274784f4df
Maxell Milay <milaymaxell@gmail.com>
2024-06-07 02:33:35 +0800

    [Feature][Maxell] use analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
                 ) : (
                     <>{children}</>
                 )}
+                <Analytics />
             </body>
         </html>
     );
